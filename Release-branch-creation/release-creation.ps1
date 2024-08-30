d31fcdd18b6dfcd93ee6d1f4525ece6a2900009a
Linu Joseph <38865727+Linu09@users.noreply.github.com>
2024-08-30 09:38:02 +0400

    Create release-creation.ps1

---
+++ Release-branch-creation/release-creation.ps1
@@ -0,0 +1,15 @@
+# Write your PowerShell commands here.
+
+$newBranchName = "release/Release-$(major_version).$(minor_version).$(patch_version)"
+$baseBranch = "$(Release.Artifacts._DHRE-Salesforce-test.SourceBranch)" # Base branch from the artifact
+
+git clone https://$(PAT)@dev.azure.com/$(Org)/$(project)/_git/$(repo)
+
+cd $(repo)
+
+git checkout $baseBranch
+
+git checkout -b $newBranchName
+
+git push origin $newBranchName
+
